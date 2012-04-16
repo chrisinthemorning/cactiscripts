4b04e387b38930dbffa3e0ca0024e2a9797bb45e
andywhite@gmail.com <andywhite@gmail.com@bb55a64a-f0d3-a7ef-3153-3355ffc1f846>
2012-04-16 14:47:15 +0000

    use generic update.snmpinterfaces.sh

--- trunk/autoadd/add.ns.sh
+++ trunk/autoadd/add.ns.sh
@@ -10,15 +10,15 @@ hostid=`./add_graphs.php  --list-hosts | grep $3 | cut -f1`
 #add node to the tree
 dcid=`echo $3 | cut -b 1-4 `
 treeid=`./add_tree.php --list-trees | grep -i $dcid | cut -f1`
-parentnode=`./add_tree.php --list-nodes --tree-id=$treeid | grep 'Load Balancers'  | cut -f2`
+parentnode=`./add_tree.php --list-nodes --tree-id=$treeid | grep 'Load Balancers Source'  | cut -f2`
 
 ./add_tree.php --type=node --node-type=host --tree-id=$treeid --parent-node=$parentnode --host-id=$hostid
 
 #add basic graphs
 ./add_graphs.php --quiet --list-graph-templates --host-template-id=$templateid | cut -f1 | xargs -I {} ./add_graphs.php --graph-type=cg --graph-template-id={} --host-id=$hostid
 
+#add snmp data for interfaces , like discards and errors
+./update.snmpinterfaces.sh $hostid $templateid
+
 #add dynamic graphs like vserver stats
 ./update.ns.sh $hostid $templateid
-
-#add snmp data for interfaces , like discards and errors
-./update.ns.snmpinterfaces.sh $hostid $templateid
