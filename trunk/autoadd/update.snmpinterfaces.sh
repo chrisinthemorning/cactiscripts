bf67c4b9a0018527d4a0c265ccccd28f5d8ce018
andywhite@gmail.com <andywhite@gmail.com@bb55a64a-f0d3-a7ef-3153-3355ffc1f846>
2012-04-25 22:01:47 +0000

    update

--- trunk/autoadd/update.snmpinterfaces.sh
+++ trunk/autoadd/update.snmpinterfaces.sh
@@ -15,7 +15,7 @@ do
 for j
   in "${snmpquerytypeidarray[@]}"
 	do
-		graphtemplateid=(`./add_graphs.php --quiet  --list-graph-templates --snmp-query-type-id=$i |  grep "^$j\b" |cut -f2`)
+		graphtemplateid=(`./add_graphs.php --quiet  --list-graph-templates --snmp-query-id=$i |  grep "^$j\b" |cut -f2`)
 		for k in "${listsnmpfieldsarray[@]}"
         	do
 			./add_graphs.php --quiet --list-snmp-values --host-id=$hostid --snmp-query-id=$i --snmp-field=$k | grep -i -v down | cut -f1 | xargs  -d "\n\r"  -I {}  bash -c " ./add_graphs.php --graph-type=ds --graph-template-id=$graphtemplateid  --host-id=$hostid --snmp-query-id=$i --snmp-query-type-id=$j --snmp-field=$k --snmp-value=\"{}\""
