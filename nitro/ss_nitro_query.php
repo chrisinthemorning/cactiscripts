bb421e852ec4756af5ad68a3884a163bc87464cb
andywhite <andywhite@gmail.com>
2012-11-23 10:33:06 +0000

    d

--- nitro/ss_nitro_query.php
+++ nitro/ss_nitro_query.php
@@ -1,60 +1,64 @@
-<?PHP
+?PHP
+// wget -O- --user nsroot --password nsroot http://192.168.255.56/nitro/v1/stat/lbvserver | tr ',' '\n'
 
-$no_http_headers = true;
+// jSON URL which should be requested
 
-/* display No errors */
-error_reporting(E_ERROR);
+//$json_url = 'http://192.168.255.56/nitro/v1/stat/lbvserver';
+//$json_url = 'http://192.168.255.56/nitro/v1/stat/protocoltcp';
+ 
+//$username = 'nsroot';  // authentication
+//$password = 'nsroot';  // authentication
 
-include_once(dirname(__FILE__) . "/../include/config.php");
-include_once(dirname(__FILE__) . "/../lib/snmp.php");
+$host     = $argv[1];
+$username = $argv[2];
+$password = $argv[3];
+$nitroapi = $argv[4];
+$json_url = "http://$host/nitro/v1/stat/$nitroapi/";
+ 
+// Curl
+$ch = curl_init( $json_url );
+$options = array(
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_BINARYTRANSFER => true,
+CURLOPT_SSL_VERIFYPEER => false,
+CURLOPT_USERPWD => $username . ":" . $password,   // authentication
+);
+curl_setopt_array( $ch, $options );
+$result =  curl_exec($ch); // Getting jSON result string
 
-require "/usr/share/cacti/resource/script_server/predis/autoload.php";
-Predis\Autoloader::register();
-$gredis = new Predis\Client();
+// Json
+$json_result=json_decode($result,true);
 
-if (!isset($called_by_script_server)) {
-        array_shift($_SERVER["argv"]);
-        print call_user_func_array("ss_nitro", $_SERVER["argv"]);
-}
+// Memcache
+//$memcache = new Memcache;
+//$memcache->connect('localhost', 11211) or die ("Could not connect");
+//$memcache->set('key', $json_result, false, 10) or die ("Failed to save data at the server");
+//$get_result = $memcache->get('key');
 
-function ss_nitro($host, $username, $password, $nitroapi, $cmd, $arg1 = "", $arg2 = "", $arg3 = "") {
-    $redis = $GLOBALS['gredis'];
-    $json_url = "http://$host/nitro/v1/stat/$nitroapi/";
-    // Curl
-    if (!$redis->exists($json_url)) {
-        $ch = curl_init( $json_url );
-        $options = array(CURLOPT_RETURNTRANSFER => true,CURLOPT_BINARYTRANSFER => true,CURLOPT_SSL_VERIFYPEER => false,CURLOPT_USERPWD => $username . ":" . $password,);
-        curl_setopt_array( $ch, $options );
-        $result =  curl_exec($ch);
-        $redis->set($json_url,$result);
-        $redis->expire($json_url,60);
-    } else {
-    	$result=$redis->get($json_url);
-    }
-    $json_result=json_decode($result,true);
-    foreach ($json_result as $key1 => $val1) {
-		if (is_array($val1)) {
-			if ($cmd == 'num_indexes') {
-				echo count($val1);
-			} else {
-				foreach($val1 as $key2 => $val2) {
-					if (is_array($val2)) {
-						if($cmd == 'get' && $arg3 == $val2[$arg1] && !is_null($val2[$arg2]) ) {
-							return $val2[$arg2];
-						} elseif ($cmd == 'query' ) {
-							print $val2[$arg1] . "!" . $val2[$arg2] . "\n";
-						} elseif ($cmd == 'index' && !is_null($val2[$arg1]) ) {
-							print $val2[$arg1] . "\n";
-						}
-					} else {
-						if (is_numeric($val2)) {
-							$prettyval2 = sprintf("%.0f", $val2);
-							echo "$key2:$prettyval2 ";
-						}
+// Split out json kvpairs into memcache kvpairs
+foreach ($json_result as $key1 => $val1) { // This will search in the 2 jsons
+  if (is_array($val1)) {
+	  	if ($argv[5] == 'num_indexes') {
+	  		echo count($val1);
+	  	} else {
+	     foreach($val1 as $key2 => $val2) {
+	//			if (is_array($value) && $value['name'] == 'blah3') {
+				if (is_array($val2)) {
+					if($argv[5] == 'get' && $argv[8] == $val2[$argv[6]] && !is_null($val2[$argv[7]]) ) {
+						echo $val2[$argv[7]];
+					} elseif ($argv[5] == 'query' ) {
+                        echo $val2[$argv[6]] . "!" . $val2[$argv[7]] . "\n";
+					} elseif ($argv[5] == 'index' && !is_null($val2[$argv[6]]) ) {
+						echo $val2[$argv[6]] . "\n";
+					}
+				} else {
+					if (is_numeric($val2)) {
+						 $prettyval2 = sprintf("%.0f", $val2);
+						 echo "$key2:$prettyval2 ";
 					}
 				}
 			}
-		}
-	}
+	    }
+    } 
 }
-?>
+// Output
